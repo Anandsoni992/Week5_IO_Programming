4fc14398526978c883bf232527ba16dd5274e509
Anandsoni992 <anandsoni9165@gmail.com>
2025-02-19 14:42:54 +0530

    Anand Add . Java object into Json Format

--- Day2/src/main/java/com/ioprogramming/practiceproblems/jsonobjecttoformat/Car.java
+++ Day2/src/main/java/com/ioprogramming/practiceproblems/jsonobjecttoformat/Car.java
@@ -1,4 +1,52 @@
 package com.ioprogramming.practiceproblems.jsonobjecttoformat;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class Car {
+    private String brand;
+    private String model;
+    private int year;
+
+    public Car() {
+    }
+
+    public Car(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String toJson() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(this);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Car car = new Car("Toyota", "Camry", 2023);
+        System.out.println(car.toJson());
+    }
 }
